782c6d246739dc1568ef7eb2ac8e4eb1f38edcc4
shuzo <zo.shu.0414.73@gmail.com>
2021-06-03 01:07:13 +0900

    update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,4 +9,25 @@ class ProfilesController < ApplicationController
         @profile = current_user.build_profile
     end
 
+    def update
+        @profile = current_user.build_profile(profile_params)
+        if @profile.save
+            redirect_to profile_path, notice: 'プロフィールを更新！'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :birthday,
+            :subscribed
+        )
+    end
+
 end
