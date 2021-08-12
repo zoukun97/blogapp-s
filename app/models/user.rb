eb282c65b31c9159abf5c61eadfec8f5892dea90
shuzo <zo.shu.0414.73@gmail.com>
2021-08-13 01:18:35 +0900

    followの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,11 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -44,6 +49,10 @@ class User < ApplicationRecord
     #   self.email.split('@').first
     # end
     profile&.nickname || self.email.split('@').first
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
   end
 
   def prepare_profile
