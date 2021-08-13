1921403a28033517d738f7aa1ca6525366e5a654
shuzo <zo.shu.0414.73@gmail.com>
2021-08-13 14:39:55 +0900

    プロフィールページを作成

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,6 +2,9 @@ class AccountsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user == current_user
+      redirect_to profile_path
+    end
   end
   
 end
