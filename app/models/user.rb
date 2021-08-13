7ee9c9fb4a71265d1d58f9726dc76550f03bb14e
shuzo <zo.shu.0414.73@gmail.com>
2021-08-13 12:10:55 +0900

    followerの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,8 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship',dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
 
   has_one :profile, dependent: :destroy
 
